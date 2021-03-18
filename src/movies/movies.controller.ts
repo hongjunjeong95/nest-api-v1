d7b55da4bfe41ff5812ede788eb9b5ba2e19ce99
zpskek <wjdghdwns0@gmail.com>
2021-03-19 05:05:58 +0900

    Movies Service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,51 +1,47 @@
 import {
   Controller,
   Get,
-  Param,
   Post,
   Delete,
   Patch,
+  Param,
   Body,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 // In nest.js, if you want something, you have to ask for it
 
 @Controller('movies')
 export class MoviesController {
-  @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
+  constructor(private readonly moviesService: MoviesService) {}
 
-  @Get('search')
-  search(@Query('year') seachingYear: string) {
-    return `We are searching for a movie made after: ${seachingYear}`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   // Delete('/:id') 안에 있는 id와 @Param('id')에 id는 같아야 한다.
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   // Put은 모든 리소스를 업데이트하기 때문에 필요한 부분만 업데이트 하기 위해서
   // Patch를 사용한다.
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
